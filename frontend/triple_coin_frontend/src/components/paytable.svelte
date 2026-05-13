059035aa248af382268e018470ff99d9042f0a3e
romant04 <roman.tarnai.04@gmail.com>
2026-05-13 15:38:02 +0200

    paytable

--- frontend/triple_coin_frontend/src/components/paytable.svelte
+++ frontend/triple_coin_frontend/src/components/paytable.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
   import front from '../assets/game/front.png';
   import back from '../assets/game/back.png';
+  import sideProfile from '../assets/game/side.png';
 
   const PAYTABLE = [
-    { combination: 'HHH', payout: 3.85 },
-    { combination: 'HHT', payout: 1.15 },
-    { combination: 'HTT', payout: 0.1 },
+    { combination: 'SSS', payout: 50 },
+    { combination: 'HHH', payout: 4 },
+    { combination: 'HH', payout: 1.5 },
+    { combination: 'H', payout: 0.5 },
   ];
 </script>
 
 <div class="flex flex-col items-center gap-3">
   <div class="flex flex-col w-max gap-2 bg-[#003075]/80 p-6 rounded-md">
     {#each PAYTABLE as row}
-      <div class="flex items-center gap-4">
+      <div class="flex items-center justify-between gap-4">
         <div class="flex gap-1">
           {#each row.combination.split('') as side}
             <img
-              src={side === 'H' ? front : back}
+              src={side === 'H' ? front : 'T' ? back : sideProfile}
               alt={side}
               class="w-12 h-12"
             />
